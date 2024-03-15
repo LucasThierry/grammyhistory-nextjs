3d6f5f80c539b1571be4f25bd3c35fae731ad11e
LucasThierry <lucasthierryn7@gmail.com>
2024-03-15 11:16:10 -0300

    Disable dark mode

--- components/mode-toggle.tsx
+++ components/mode-toggle.tsx
@@ -7,7 +7,6 @@ export function ModeToggle() {
 
   return (
     <button
-      onClick={() => setTheme(theme === "light" ? "dark" : "light")}
       className="border rounded-md w-6 h-6 flex items-center justify-center">
       <span className="sr-only">Toggle mode</span>
       {theme !== "dark" ? (

01b680a5ae949859739f4376e9fdf09dd7ddd791
LucasThierry <lucasthierryn7@gmail.com>
2024-03-14 23:41:50 -0300

    Fixed requirements

--- config-overrides.js
+++
@@ -1,3 +0,0 @@
-const TerserPlugin = require('terser-webpack-plugin');
-
-config.optimization = { minimize: true, minimizer: [new TerserPlugin({ exclude: /\/node_modules\/react-image-gallery/ })], };
